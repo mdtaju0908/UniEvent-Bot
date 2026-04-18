854f5b6eea44d333a3042d1dd5a09d169e17572a
Md Taju <er.mdtaju@gmail.com>
2026-04-18 16:07:08 +0530

    Add files via upload

---
+++ backend/controllers/announcementController.js
@@ -0,0 +1,20 @@
+const Announcement = require('../models/Announcement');
+
+exports.sendAnnouncement = async (req, res) => {
+  try {
+    const announcement = new Announcement(req.body);
+    await announcement.save();
+    res.status(201).json(announcement);
+  } catch (err) {
+    res.status(500).json({ message: 'Server Error' });
+  }
+};
+
+exports.getLiveAnnouncements = async (req, res) => {
+  try {
+    const announcements = await Announcement.find().sort({ createdAt: -1 });
+    res.json(announcements);
+  } catch (err) {
+    res.status(500).json({ message: 'Server Error' });
+  }
+};
